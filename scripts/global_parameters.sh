2dbb36aa02a122779ebcf257f770fa8d4e9a8797
Thanos (Athanasios) Dimitriadis <athanasios.dimitriadis.16@ucl.ac.uk>
2019-01-20 20:33:47 +0000

    added usage information

--- scripts/global_parameters.sh
+++ scripts/global_parameters.sh
@@ -5,24 +5,24 @@
 # Set the directory of the project
 export DIR=/mnt/disk3_36TB/Thanos_sequencing/promoter_batch_1
 
-# Set the path to sample_names.txt file (read the instructions first!)
+# *** Set the path to sample_names.txt file (read the instructions first!)
 export sample_names=$DIR/sample_names.txt
 
 #### Docker ####
 
-# Set name of docker samtools image
+# *** Set name of docker samtools image
 export samtools_image=biocontainers/samtools:v1.7.0_cv3
 
-# Set name of docker ONT guppy image
+# *** Set name of docker ONT guppy image
 export guppy=athanadd/ont-guppy:2.1.3
 
-# Set name of docker minimap2 image
+# *** Set name of docker minimap2 image
 export minimap2_image=athanadd/minimap2:2.13
 
-# Set name of docker nanopolish image
+# *** Set name of docker nanopolish image
 export nanopolish_image=athanadd/nanopolish:0.10.2
 
-# Set name of docker sniffles image
+# *** Set name of docker sniffles image
 export sniffles_image=quay.io/biocontainers/sniffles:1.0.10--hd4ff3c4_0
 
 # Set docker disk mounting options (check docker manual if unsure)
@@ -36,10 +36,10 @@ export gen_range_chr=chr20
 export gen_range_start=4685200
 export gen_range_end=4701800
 
-# Set q-score quality cutoff when basecalling with Guppy (default: 7.0)
+# *** Set q-score quality cutoff when basecalling with Guppy (default: 7.0)
 export q_score=7.0
 
-# Set path to R library files
+# *** Set path to R library files
 export libs_path=$DIR/R_lib
 
 # Set genome fasta file (to fix MD headers of reads)
@@ -51,7 +51,7 @@ export index=/mnt/disk3_36TB/Thanos_sequencing/required_files/etc/minimap2/hg38.
 # Set path to raw fast5 folder
 export raw_fast5_folder=/mnt/win/Thanos/PRNP_sequencing/promoter_batch_1/20190109_1455_MN28445_FAK45966_0d3c4853/fast5
 
-# Set minimum length of structural variants to be reported by sniffles
+# *** Set minimum length of structural variants to be reported by sniffles
 export sniffles_min_var_length=20
 
 # Set minimum read length accepted by sniffles (default: 2000)
@@ -62,7 +62,7 @@ export min_read_length=2000
 # Set number of threads to be used for the proccess of each sample in various multithreaded operations
 export thrds=15
 
-# Set number of parallel jobs for nanopolish variants (each job requires a lot of RAM. Up to 20-30Gb for a 1Gb fastq file)
+# *** Set number of parallel jobs for nanopolish variants (each job requires a lot of RAM. Up to 20-30Gb for a 1Gb fastq file)
 export nanopolish_variants_jobs=2
 
 # Set basecalling worker threads
